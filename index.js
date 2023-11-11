232f0a8e3a8adb1c3ca54465b3ba2b62a3f7a1b2
jignesh sharma <jigneshsharma9868@gmail.com>
2023-11-11 06:44:01 +0530

    extracting the style and js to the head section

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const port = process.env.PORT || 3000;
 const expressLayouts = require('express-ejs-layouts');
 app.use(express.static('./assets'));
 app.use(expressLayouts);
+//extract style and scripts from sub pages into the layout
+
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
 app.use('/',require('./routes/index'));
 
 
